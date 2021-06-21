955069236654e53576f67f90577120df17876b91
Armanul46 <arman.sovware@gmail.com>
2021-06-21 16:22:39 +0600

    added discout class

--- includes/template/template.php
+++ includes/template/template.php
@@ -19,7 +19,7 @@
         </div>
         <div class="wpcu-product__details">
             <h2 class="wpcu-product__title"><a href="<?php echo get_the_permalink(); ?>"><?php echo get_the_title(); ?></a></h2>
-            <p class="wpcu-product__price wpcu-product__price--discount">
+            <p class="wpcu-product__price <?php ! empty( $sale_price ) ? 'wpcu-product__price--discount' : ''; ?>">
                 <?php 
                 if(  empty( $sale_price ) ) { ?>
                 <span>$<?php echo $product->get_regular_price(); ?></span> 
