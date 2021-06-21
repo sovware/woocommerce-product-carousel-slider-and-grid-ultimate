a68d18a673f40a4ba9ead5882420a0abe1e96232
Armanul46 <arman.sovware@gmail.com>
2021-06-21 15:11:06 +0600

    dynamic

--- includes/template/template.php
+++ includes/template/template.php
@@ -1,40 +1,43 @@
-
-<div class='main' id='theme_1'>
-<?php
- while($loop->have_posts()) : $loop->the_post();
- $aazz_thumb = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()), 'large' );
- $wpcsu_img = $aazz_thumb['0'];
- ?>
-    <?php  if ( has_post_thumbnail( $loop->post->ID ) ) { ?>
-    <div class="cards">
-    <div class='image'>
-        <a href="<?php echo get_the_permalink(); ?>">
-            <img src="<?php esc_url($wpcsu_img); ?>" alt="">
-        </a>
-    </div>
-    <?php } ?>
-    <div class='title'>
-        <a href="<?php echo get_the_permalink(); ?>"><?php echo get_the_title(); ?></a>
-    </div>
-    <div class='cart'> <?php echo do_shortcode('[add_to_cart id="'.get_the_ID().'"]');?>
-    </div>
- </div>
- <?php
-endwhile; ?>
-</div>
-
 <div class="wpcu-products" id="theme-one">
+    <?php
+    
+    while($loop->have_posts()) : $loop->the_post();
+    global $post,$product;
+    $aazz_thumb = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()), 'large' );
+    $wpcsu_img = $aazz_thumb['0'];
+    $sale_price = $product->get_sale_price();
+    $ratings = (($product->get_average_rating()/5)*100);
+    ?>
     <div class="wpcu-product">
         <div class="wpcu-product__img">
-            <img src="https://via.placeholder.com/250x250" alt="">
+            <a href="<?php echo get_the_permalink(); ?>">
+                <img src="<?php esc_url($wpcsu_img); ?>" alt="">
+            </a>
             <div class="overlay-content-bottom">
-                <a href="" class="wpcu-btn wpcu-btn-primary">Add to cart</a>
+            <?php echo do_shortcode('[add_to_cart id="'.get_the_ID().'"]');?>
             </div>
         </div>
         <div class="wpcu-product__details">
-            <h2 class="wpcu-product__title">Product Title One</h2>
-            <p class="wpcu-product__price wpcu-product__price--discount"><s>$25.00</s> <span>$20.00</span></p>
-            <div class="wpcu-product__rating">rating stars</div>
+            <h2 class="wpcu-product__title"><a href="<?php echo get_the_permalink(); ?>"><?php echo get_the_title(); ?></a></h2>
+            <p class="wpcu-product__price wpcu-product__price--discount">
+                <s><?php echo $product->get_price_html(); ?></s> 
+                <?php  
+                if( ! empty( $sale_price ) ) { ?>
+                <span><?php echo $this->aazz_show_discount_percentage(); ?></span>
+                <?php } ?>
+            </p>
+            <div class="wpcu-product__rating">
+                <div class="atw_rating woocommerce">
+                    <div class="woocommerce-product-rating">
+                        <div class="star-rating" title="<?php echo $ratings; ?>%">
+                            <span style="width: <?php echo $ratings; ?>%;"></span>
+                        </div>
+                        <span class="total-rating">(<?php echo $product->get_rating_count();?>)</span>
+                    </div>
+                </div>
+            </div>
         </div>
     </div><!-- ends: .wpcu-product -->
+    <?php
+endwhile; ?>
 </div><!-- ends: .wpcu-products -->
