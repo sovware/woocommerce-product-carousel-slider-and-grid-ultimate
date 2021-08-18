e6cc5bacac2d22524db5d427a4d1f06ca3c2700c
Armanul46 <arman.sovware@gmail.com>
2021-08-18 08:10:43 +0600

    dynamic dot pagination

--- includes/template/pagination.php
+++ includes/template/pagination.php
@@ -1,6 +1,13 @@
+<?php
+if( 'grid' == $layout ) {
+?>
 <!-- .wpcu-pagination--left/.wpcu-pagination--right -->
 <div class="wpcu-pagination">
     <?php
     echo wcpcsu_pagination( $loop, $paged );
     ?>
 </div>
+<?php } else { ?>
+<!-- wpcu-carousel-pagination -->
+<div class="wpcu-carousel-pagination"></div>
+<?php } ?>
