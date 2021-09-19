40daf26a183cbb52c738292f56e070bea7ccd47b
Armanul46 <arman.sovware@gmail.com>
2021-09-19 14:36:33 +0600

    fixed undifined variable issue

--- includes/classes/class-shortcode.php
+++ includes/classes/class-shortcode.php
@@ -338,7 +338,8 @@ class wcpcsu_Shortcode
             } else {
 
                 $max_percentage = 0;
-
+                $percentage = '';
+                
                 foreach ( $product->get_children() as $child_id ) {
                     $variation = wc_get_product( $child_id );
                     $price = $variation->get_regular_price();
