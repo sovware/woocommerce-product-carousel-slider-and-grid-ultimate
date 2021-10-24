b8bbf711b61a95763108fc0bce5f7f821a43dd7a
Shamim Ahmed <shamimeforb@gmail.com>
2021-10-24 09:09:53 +0600

    Removed: img to svg js code and added raw svg icons

--- includes/helper-functions.php
+++ includes/helper-functions.php
@@ -16,8 +16,8 @@ if (!function_exists('wcpcsu_pagination')) {
             'format' => '?paged=%#%',
             'current' => max( 1, $paged ),
             'total' => $custom_post_query->max_num_pages,
-            'prev_text' => apply_filters('wcpcsu_pagination_prev_text', '<img src="' .  WCPCSU_URL . 'assets/icons/arrow-left.svg" alt="" class="wpcu-svg">'),
-            'next_text' => apply_filters('wcpcsu_pagination_next_text', '<img src="' .  WCPCSU_URL . 'assets/icons/arrow-right.svg" alt="" class="wpcu-svg">'),
+            'prev_text' => apply_filters('wcpcsu_pagination_prev_text', '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="6" height="auto" preserveAspectRatio="xMidYMid meet" viewBox="0 0 576 1024" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);"><path d="M528 0q14 0 24 10q4 3 6 7t3 8.5t1 9t-1 8.5t-3 8t-6 8L96 515l450 450q6 6 8.5 15t0 18t-8.5 15q-10 10-24.5 10t-24.5-10L23 539q-10-10-10-24t10-24L504 10q10-10 24-10z"/><rect x="0" y="0" width="576" height="1024" fill="rgba(0, 0, 0, 0)" /></svg>'),
+            'next_text' => apply_filters('wcpcsu_pagination_next_text', '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="6" height="auto" preserveAspectRatio="xMidYMid meet" viewBox="0 0 576 1024" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);"><path d="M48 1023q-14 0-24.5-10T13 989t10-24l457-457L30 59q-6-7-8.5-16t0-17.5T30 10Q40 0 54.5 0T79 10l473 474q5 5 7.5 11.5t2.5 13t-2.5 12.5t-7.5 11L72 1013q-5 5-11.5 7.5T48 1023z"/><rect x="0" y="0" width="576" height="1024" fill="rgba(0, 0, 0, 0)" /></svg>'),
         ) );
         if( $links ) {
             $navigation = _navigation_markup($links, 'pagination', ' ');
