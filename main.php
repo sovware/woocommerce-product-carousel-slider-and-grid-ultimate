674c0b4f7e0c663e5838cb29ad8bbebbbbe97931
Armanul46 <arman.sovware@gmail.com>
2021-11-22 13:17:23 +0600

    removed notice

--- main.php
+++ main.php
@@ -74,9 +74,9 @@ if( ! in_array('woocommerce-product-carousel-slider-grid-ultimate-pro/main.php',
                 add_action('admin_enqueue_scripts',array(self::$instance, 'wcpcsu_enqueue_file'));
                 add_action('template_redirect',array(self::$instance, 'template_enqueue_file'));
                 add_action('admin_menu',array(self::$instance,'upgrade_to_pro'));
-                if( empty( get_option('wcpcsu_dismiss_notice') ) ) {
+                /* if( empty( get_option('wcpcsu_dismiss_notice') ) ) {
                     add_action( 'admin_notices', array( self::$instance, 'admin_notices') );
-                }
+                } */
                 self::$instance->wcpcsu_include();
                 self::$instance->custom_post = new Wcpcsu_Custom_Post();
                 self::$instance->metabox = new Wcpcsu_Meta_Box();
