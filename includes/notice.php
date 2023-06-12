ba219c6dd65717e7689240dec88714fe02dbeb98
iamibrahimriaz <ibrahimriaz.sovware@gmail.com>
2023-06-12 11:35:06 +0600

    Admin Notice on Exclusive Offer - Updated

--- includes/notice.php
+++ includes/notice.php
@@ -12,14 +12,20 @@ if ( ! empty( $_GET['wcpcsu-dismiss-discount-notice'] ) && 'true' == $_GET['wcpc
 
 if ( ! isset( $_GET['wcpcsu-dismiss-discount-notice'] ) ) { ?>
     <div class="wpcu-dashboard-notice">
-        <a class="wpcu-dashboard-notice__dismiss" href="<?php echo esc_url( wp_nonce_url( add_query_arg( 'wcpcsu-dismiss-discount-notice', 'true' ) ) ); ?>"><?php esc_html_e( 'Dismiss', 'woocommerce-product-carousel-slider-and-grid-ultimate' ); ?></a>
+        <a class="wpcu-dashboard-notice__dismiss wpcu-dashboard-notice__close" href="<?php echo esc_url( wp_nonce_url( add_query_arg( 'wcpcsu-dismiss-discount-notice', 'true' ) ) ); ?>"><?php esc_html_e( 'x', 'woocommerce-product-carousel-slider-and-grid-ultimate' ); ?></a>
 
         <img src="<?php echo esc_url( 'https://s12.gifyu.com/images/SuUGf.gif' ); ?>" alt="">
-        <h5><?php esc_html_e( 'EXCLUSIVE OFFER FOR PRODUCT CAROUSEL SLIDER & GRID ULTIMATE!', 'logo-showcase-ultimate' ); ?></h5>
-        <p>
-        <?php esc_html_e( "Save 35% this summer with Product Carousel Slider & Grid Ultimate! Boost your WooCommerce store this season and enhance product showcasing. Create dynamic sliders and grids, customize layouts, and maximize conversions. Don't miss out on this exclusive offer.", "woocommerce-product-carousel-slider-and-grid-ultimate" ); ?>
-        </p>
-        <a class="wpcu-dashboard-notice__dismiss" target="_blank" href="<?php echo esc_url( 'https://wpwax.com/product/woocommerce-product-carousel-slider-grid-ultimate-pro/#single-plugin-pricing-plan' ); ?>"><?php esc_html_e( 'Get Now!', 'woocommerce-product-carousel-slider-and-grid-ultimate' ); ?></a>
-        
+        <div class="wpcu-dashboard-notice__content">
+            <h5><?php esc_html_e( 'EXCLUSIVE OFFER FOR PRODUCT CAROUSEL SLIDER & GRID ULTIMATE!', 'woocommerce-product-carousel-slider-and-grid-ultimate' ); ?></h5>
+            <p>
+                <?php
+                    $offer = '<strong>' . esc_html__( "Save 35% this summer with Product Carousel Slider & Grid Ultimate! ", "woocommerce-product-carousel-slider-and-grid-ultimate" ) . '</strong>';
+                    $text = esc_html__( "Boost your WooCommerce store this season and enhance product showcasing. Create dynamic sliders and grids, customize layouts, and maximize conversions. Don't miss out on this exclusive offer.", "woocommerce-product-carousel-slider-and-grid-ultimate" );
+
+                    echo $offer . $text;
+                ?>
+            </p>
+            <a class="wpcu-dashboard-notice__dismiss wpcu-dashboard-notice__btn" target="_blank" href="<?php echo esc_url( 'https://wpwax.com/product/woocommerce-product-carousel-slider-grid-ultimate-pro/#single-plugin-pricing-plan' ); ?>"><?php esc_html_e( 'Get Now!', 'woocommerce-product-carousel-slider-and-grid-ultimate' ); ?></a>
+        </div>
     </div>
 <?php } ?>
